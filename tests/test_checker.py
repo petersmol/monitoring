dbd56bf9be542e912a68ff51769b81f82b7e32b2
Peter Smolovich <pub@petersmol.ru>
2021-01-13 22:32:58 +0300

    fix test_checker

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,7 +1,9 @@
 import pytest
 import monitoring.checker
 import monitoring.settings
+from monitoring.check_models import CheckParams
 from unittest.mock import MagicMock
+import redis
 
 
 def test_checker_run(mocker):
@@ -11,13 +13,17 @@ def test_checker_run(mocker):
     sender.assert_called_once_with()
 
     # Mocking every calls and execute checker.run()
-    mocker.patch("monitoring.checker.Checker.get_check_list", return_value=["dummy"])
+    check_param = CheckParams(url="dummy")
+    mocker.patch(
+        "monitoring.checker.Checker.get_check_list", return_value=[check_param]
+    )
     mocker.patch("monitoring.checker.Checker.perform_check", return_value="dummy2")
+    mocker.patch("redis.Redis.from_url")
     checker.run()
 
     # Checking that mocks are still actual
     monitoring.checker.Checker.get_check_list.assert_called_once_with()
-    monitoring.checker.Checker.perform_check.assert_called_once_with("dummy")
+    monitoring.checker.Checker.perform_check.assert_called_once_with(check_param)
     checker.sender.send.assert_called_once_with("dummy2")
 
 
@@ -26,7 +32,7 @@ def test_checker_get_check_list(mocker):
 
     monitoring.settings.cfg["checks"] = [{"url": "test-url"}]
     check_list = checker.get_check_list()
-    assert check_list[0]["url"] == "test-url"
+    assert check_list[0].url == "test-url"
 
 
 def test_checker_perform_check_200_ok(mocker):
@@ -34,15 +40,15 @@ def test_checker_perform_check_200_ok(mocker):
     checker = monitoring.checker.Checker(sender=mocker.Mock())
     get_result = MagicMock()
 
-    check_param = {
-        "url": "my-url",
-    }
+    check_param = CheckParams(
+        url="my-url",
+    )
     get_result.status_code = 200
 
     mocker.patch("requests.get", return_value=get_result)
     check_list = checker.perform_check(check_param)
     assert check_list.success == True
-    assert check_list.url == check_param["url"]
+    assert check_list.url == check_param.url
     assert check_list.expected_code == 200
     assert check_list.regexp == ""
 
@@ -52,9 +58,9 @@ def test_checker_perform_check_502_fail(mocker):
     checker = monitoring.checker.Checker(sender=mocker.Mock())
     get_result = MagicMock()
 
-    check_param = {
-        "url": "my-url",
-    }
+    check_param = CheckParams(
+        url="my-url",
+    )
     get_result.status_code = 502
 
     mocker.patch("requests.get", return_value=get_result)
@@ -67,10 +73,10 @@ def test_checker_perform_check_200_fail(mocker):
     checker = monitoring.checker.Checker(sender=mocker.Mock())
     get_result = MagicMock()
 
-    check_param = {
-        "url": "my-url",
-        "expected_code": 302,
-    }
+    check_param = CheckParams(
+        url="my-url",
+        expected_code=302,
+    )
     get_result.status_code = 200
 
     mocker.patch("requests.get", return_value=get_result)
@@ -83,17 +89,17 @@ def test_checker_perform_check_regexp_ok(mocker):
     checker = monitoring.checker.Checker(sender=mocker.Mock())
     get_result = MagicMock()
 
-    check_param = {
-        "url": "my-url",
-        "regexp": "J.ck",
-    }
+    check_param = CheckParams(
+        url="my-url",
+        regexp="J.ck",
+    )
     get_result.status_code = 200
     get_result.content = "All work and no play makes Jack a dull boy"
 
     mocker.patch("requests.get", return_value=get_result)
     check_list = checker.perform_check(check_param)
     assert check_list.success == True
-    assert check_list.regexp == check_param["regexp"]
+    assert check_list.regexp == check_param.regexp
     assert check_list.response_length == len(get_result.content)
 
 
@@ -102,15 +108,15 @@ def test_checker_perform_check_regexp_ok(mocker):
     checker = monitoring.checker.Checker(sender=mocker.Mock())
     get_result = MagicMock()
 
-    check_param = {
-        "url": "my-url",
-        "regexp": "J.ck",
-    }
+    check_param = CheckParams(
+        url="my-url",
+        regexp="J.ck",
+    )
     get_result.status_code = 200
     get_result.content = "All work and no play makes James a dull boy"
 
     mocker.patch("requests.get", return_value=get_result)
     check_list = checker.perform_check(check_param)
     assert check_list.success == False
-    assert check_list.regexp == check_param["regexp"]
+    assert check_list.regexp == check_param.regexp
     assert check_list.response_length == len(get_result.content)
